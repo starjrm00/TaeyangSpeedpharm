35537bc8cbd8c6813587c59d77eea54dfc06e597
unknown <starjrm00@gmail.com>
2026-01-14 07:27:11 +0900

    fix error at logic4

--- Firebase_upload.py
+++ Firebase_upload.py
@@ -1,31 +1,32 @@
 from Firebase_connect import db
 
 def upload_new_data(row):
-    doc_id = f"{row['거래처']}_{row['상품명']}_{row['규격']}"
-    product_ref = db.collection("Product").document(doc_id)
+    for _, row in df.iterrows():
+        doc_id = f"{row['거래처']}_{row['상품명']}_{row['규격']}"
+        product_ref = db.collection("Product").document(doc_id)
 
-    product_ref.set({
-        "거래처": row["거래처"],
-        "상품명": row["상품명"],
-        "규격": row["규격"],
-        "출고가": row["출고가"],
-        "입고가": row["입고가"],
-        "기준약가": row["기준약가"],
-        "단위": row["단위"]
-    }, merge = True)
-    '''
-    date_str = str(row["날짜"].date())
+        product_ref.set({
+            "거래처": row["거래처"],
+            "상품명": row["상품명"],
+            "규격": row["규격"],
+            "출고가": row["출고가"],
+            "입고가": row["입고가"],
+            "기준약가": row["기준약가"],
+            "단위": row["단위"]
+        }, merge = True)
+        '''
+        date_str = str(row["날짜"].date())
 
-    snapshot = product_ref.get(field_paths=["`판매량`"])    
-    existing_stock = snapshot.to_dict().get("판매량", {}) if snapshot.exists else {}
+        snapshot = product_ref.get(field_paths=["`판매량`"])    
+        existing_stock = snapshot.to_dict().get("판매량", {}) if snapshot.exists else {}
 
-    current_stock = existing_stock.get(date_str, 0)
-    existing_stock[date_str] = current_stock + row["판매량"]
+        current_stock = existing_stock.get(date_str, 0)
+        existing_stock[date_str] = current_stock + row["판매량"]
 
-    product_ref.update({
-        "판매량": existing_stock
-    })
-    '''
+        product_ref.update({
+            "판매량": existing_stock
+        })
+        '''
 
 def reduce_stock(df):
     for _, row in df.iterrows():
